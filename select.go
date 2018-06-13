6956fb87add4ea4131009938aaa0fb670ae74aea
zhanpei <zhangzhanpei@gmail.com>
2018-06-13 17:06:54 +0800

    select

--- select.go
+++ select.go
@@ -6,22 +6,24 @@ import "time"
 func main() {
     ch := make(chan bool)
 
-    //执行一个耗时任务
+    // 执行一个耗时任务
     go func () {
         fmt.Println("long time mission")
         time.Sleep(time.Duration(3) * time.Second)
         ch <- true
     }()
 
-    //select在进入时会选择一个可执行的case来执行, 如果都不可执行则阻塞
+    // select 中只要有一个 case 能执行，则立即执行
+    // 同一时刻有多个 case 可以执行，则随机执行一个
+    // 如果 case 都不可执行，则执行 default
     select {
-        //耗时任务执行完毕
+        // 耗时任务执行完毕
         case <- ch:
             fmt.Println("ok")
-        //5s后超时
+        // 5s 后超时
         case <- time.After(time.Duration(5) * time.Second):
             fmt.Println("time out")
-        //当其他case都不能执行时, 执行default
+        // 当其他 case 都不能执行时, 执行 default
         default:
             fmt.Println("default")
     }
